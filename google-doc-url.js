2ddfca8333e23332e755f6746df9d33f5645f768
Mburu-coder <60397100+Mburu-coder@users.noreply.github.com>
2022-03-03 14:11:40 +0300

    My Google API Key

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1aQJY0dt5psr_DzN4_IHPi3F7SXNWPSxVO4EMBWgXLL0/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyAmjorP-jFW1mWrkVzaN-s1f822-YG_6D8';
